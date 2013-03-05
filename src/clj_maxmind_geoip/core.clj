f938542598c5cb8072d319e026a6125cb87fdd3a
Daniel MacDougall <dmacdougall@gmail.com>
2013-03-04 17:45:58 -0800

    Function for city lookup, and add tests

--- src/clj_maxmind_geoip/core.clj
+++ src/clj_maxmind_geoip/core.clj
@@ -2,6 +2,7 @@
 ; Currently this handles only countries.
 (ns clj-maxmind-geoip.core
   (:import com.maxmind.geoip.LookupService
+           com.maxmind.geoip.regionName
            java.util.Locale))
 
 (def ^:private geoip-access-modes {:memory 1 :check 2 :index 4})
@@ -15,7 +16,24 @@
     (swap! lookup-service (constantly service))))
 
 (defn lookup-country
-  "nil if no country could be found, or {:name, :code}."
+  "Lookup country information for an IP address. Only available when querying a GeoIP Country database.
+  Returns a map of the following country info, or nil if none found:
+  {:name, :code}"
   [ip]
   (when-let [result (.getCountry @lookup-service ip)]
     {:code (.getCode result) :name (.getName result)}))
+
+(defn lookup-location
+  "Lookup location information for an IP address. Only available when querying a GeoIP City database.
+  Returns a map of the following location info, or nil if none found:
+  {:country-code, :country-name, :region-code, :region-name, :city, :postal-code, :latitude, :longitude}"
+  [ip]
+  (when-let [result (.getLocation @lookup-service ip)]
+    {:country-code (.countryCode result)
+     :country-name (.countryName result)
+     :region-code (.region result)
+     :region-name (regionName/regionNameByCode (.countryCode result) (.region result))
+     :city (.city result)
+     :postal-code (.postalCode result)
+     :latitude (.latitude result)
+     :longitude (.longitude result)}))
--- test/clj_maxmind_geoip/core_test.clj
+++ test/clj_maxmind_geoip/core_test.clj
@@ -1,6 +1,33 @@
 (ns clj-maxmind-geoip.core-test
   (:use midje.sweet
-        clj-maxmind-geoip.core))
+        clj-maxmind-geoip.core)
+  (:import java.io.File))
 
-(fact "FIXME, I fail"
-  (= 0 1) => true)
+(defn ensure-db-exists [path]
+  (when-not (-> (File. path) .exists)
+    (println "Missing required file:" path)
+    (println "Download from http://dev.maxmind.com/geoip/geolite")
+    (System/exit 1)))
+
+(def geoip-country-db "data/GeoIp.dat")
+(def geoip-city-db "data/GeoLiteCity.dat")
+
+(ensure-db-exists geoip-country-db)
+(ensure-db-exists geoip-city-db)
+
+(init-geoip geoip-country-db)
+(facts "lookup-country works"
+  (lookup-country "127.0.0.1") => {:code "--" :name "N/A"}
+  (lookup-country "64.233.160.0") => {:code "US" :name "United States"})
+
+(init-geoip geoip-city-db)
+(facts "lookup-location works"
+  (lookup-location "127.0.0.1") => nil
+  (lookup-location "64.233.160.0") => (just {:country-code "US"
+                                             :country-name "United States"
+                                             :region-code "CA"
+                                             :region-name "California"
+                                             :city "Mountain View"
+                                             :postal-code "94043"
+                                             :latitude number?
+                                             :longitude number?}))
