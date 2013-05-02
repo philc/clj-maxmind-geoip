d988d255e2dc13383259872defc80e4113cae087
Daniel MacDougall <dmacdougall@gmail.com>
2013-05-02 12:08:33 -0700

    Add type hints to allow use in performance-critical code

--- src/clj_maxmind_geoip/core.clj
+++ src/clj_maxmind_geoip/core.clj
@@ -5,30 +5,34 @@
            com.maxmind.geoip.regionName
            java.util.Locale))
 
+(set! *warn-on-reflection* true)
+
 (def ^:private geoip-access-modes {:memory 1 :check 2 :index 4})
 
 (def ^:private lookup-service (atom nil))
 
 (defn init-geoip
   "- database-path: the country edition of the maxmind geoip database."
-  [database-path]
-  (let [service (LookupService. database-path (:memory geoip-access-modes))]
+  [^String database-path]
+  (let [service (LookupService. database-path ^int (:memory geoip-access-modes))]
     (swap! lookup-service (constantly service))))
 
 (defn lookup-country
   "Lookup country information for an IP address. Only available when querying a GeoIP Country database.
   Returns a map of the following country info, or nil if none found:
   {:name, :code}"
-  [ip]
-  (when-let [result (.getCountry @lookup-service ip)]
+  [^String ip]
+  (when-let [^com.maxmind.geoip.Country result
+             (.getCountry ^com.maxmind.geoip.LookupService @lookup-service ip)]
     {:code (.getCode result) :name (.getName result)}))
 
 (defn lookup-location
   "Lookup location information for an IP address. Only available when querying a GeoIP City database.
   Returns a map of the following location info, or nil if none found:
   {:country-code, :country-name, :region-code, :region-name, :city, :postal-code, :latitude, :longitude}"
-  [ip]
-  (when-let [result (.getLocation @lookup-service ip)]
+  [^String ip]
+  (when-let [^com.maxmind.geoip.Location result
+             (.getLocation ^com.maxmind.geoip.LookupService @lookup-service ip)]
     {:country-code (.countryCode result)
      :country-name (.countryName result)
      :region-code (.region result)
